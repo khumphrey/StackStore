d4d5a871ef96c4f8128375804a7560803ce31eef
Patrick <patsneep@gmail.com>
2016-02-29 19:18:59 -0500

    changes to orderSchema

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 var orderSchema = new mongoose.Schema({
-	order: Array,
+	purchasedItems: Array,
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'User'
