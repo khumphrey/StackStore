8ea7979d09267c23013b394b0ff1385052f4bbd2
apackin <apackin@gmail.com>
2016-03-01 12:36:01 -0500

    Createed CRUD routes for products

--- server/app/routes/products/index.js
+++ server/app/routes/products/index.js
@@ -1,5 +1,7 @@
 'use strict';
-var router = require('express').Router();
+var router = require('express').Router(),
+	_ = require('lodash');
+
 var Product  = require('mongoose').model('Product');
 module.exports = router;
 
@@ -12,4 +14,39 @@ router.param('id', function(req, res, next, id) {
 		next();
 	})
 	.then(null, next);
+});
+
+router.get('/', function(req, res, next){
+	Product.find({})
+	.then(products => res.json(products))
+	.then(null, next);
+	// return .data?
+});
+
+router.get('/:id', function(req, res, next){
+	res.json(req.product);
+});
+
+//These are admin functions:
+router.post('/', function(req, res, next){
+
+	Product.create(req.body)
+	.then(product => res.status(201).json(product))
+	.then(null,next);
+});
+
+router.put('/:id', function(req, res, next){
+
+	_.extend(req.product, req.body);
+	req.product.save()
+	.then(products => res.json(products))
+	.then(null, next);
+});
+
+router.delete('/:id', function(req, res, next){
+	
+	req.product.delete()
+	.then(function(){
+		res.status(204).end();
+	}).then(null, next);
 });
