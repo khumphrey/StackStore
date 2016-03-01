02c50f52a408f77726974dac93bd8f1fa81d862c
Patrick <patsneep@gmail.com>
2016-02-29 19:16:49 -0500

    changes to orderSchema

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,22 +2,7 @@
 const mongoose = require('mongoose');
 
 var orderSchema = new mongoose.Schema({
-	products: [{
-		productId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Product',
-			required: true
-		},
-		quantity: {
-			type: Number,
-			required: true
-		},
-		// We need to remember price at the time of purchase
-		price: {
-			type: Number,
-			required: true
-		},
-	}],
+	order: Array,
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'User'
