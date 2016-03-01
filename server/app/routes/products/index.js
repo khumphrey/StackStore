a6330658bb398d1a9612113bb6de8b45ffbc5751
apackin <apackin@gmail.com>
2016-03-01 12:28:01 -0500

    added router param for id

--- server/app/routes/products/index.js
+++ server/app/routes/products/index.js
@@ -1,3 +1,15 @@
 'use strict';
 var router = require('express').Router();
+var Product  = require('mongoose').model('Product');
 module.exports = router;
+
+router.param('id', function(req, res, next, id) {
+	id = id.toString();
+	Product.findById(id)
+	.then(function (product){
+		if(!product) throw Error; 
+		req.product = product;
+		next();
+	})
+	.then(null, next);
+});
