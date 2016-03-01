595cfd3304506df6afab136c9a271361d67cdaa4
Patrick <patsneep@gmail.com>
2016-03-01 17:35:33 -0500

    more tests for reviews

--- server/app/routes/reviews/index.js
+++ server/app/routes/reviews/index.js
@@ -6,7 +6,11 @@ var router = require('express').Router();
 router.param('reviewId', function(req, res, next, id) {
 	Review.findById(id)
 		.then(function(review) {
+			if (!review) {
+				return next({status: 404, message: "Review not found"});
+			}
 			req.review = review;
+			next();
 		})
 		.then(null, function(err){
 			next({status: 404, message: "Review not found"});
@@ -15,6 +19,7 @@ router.param('reviewId', function(req, res, next, id) {
 });
 
 // Return all reviews
+// do we want to populate them?
 router.get('/', function(req, res, next) {
 	Review.find({})
 		.then(function(reviews) {
@@ -24,18 +29,48 @@ router.get('/', function(req, res, next) {
 });
 
 // Return one specific review by ID
+// do we want to populate it?
 router.get('/:reviewId', function(req, res, next) {
 	res.status(200).json(req.review);
 });
 
-// router.post()
-// TBD
 
-// router.put()
-// TBD
+// AUTHENTICATION --- ALL FOLLOWING ROUTES ARE NOT PUBLIC
+var ensureAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.status(401).end();
+    }
+};
 
-// router.delete()
-// TBD
+router.use(ensureAuthenticated);
 
+router.post('/', function(req, res, next) {
+	Review.create(req.body)
+		.then(function(newReview) {
+			res.status(201).json(newReview);
+		})
+		.then(null, next);
+});
+
+router.put('/:reviewId', function(req, res, next) {
+	Review.update(req.review, req.body)
+		.then(function() {
+			return Review.findById(req.review._id);
+		})
+		.then(function(updatedReview) {
+			res.status(200).json(updatedReview);
+		})
+		.then(null, next);
+});
+
+router.delete('/:reviewId', function(req, res, next) {
+	Review.remove(req.review)
+		.then(function() {
+			res.status(204).send();
+		})
+		.then(null, next);
+});
 
 module.exports = router;
--- tests/server/routes/reviews-test.js
+++ tests/server/routes/reviews-test.js
@@ -23,12 +23,12 @@ describe('Reviews Route', function() {
 		mongoose.connect(dbURI, done);
 	});
 
-	beforeEach('Create mock review data', function(done) {
+	var createdReviews;
+	var createdCategories;
+	var createdUsers;
+	var createdProducts;
 
-		var createdCategories;
-		var createdUsers;
-		var createdProducts;
-		var createdReviews;
+	beforeEach('Create mock review data', function(done) {
 
 		User.create([{
 			email: "bob@email.com",
@@ -36,10 +36,14 @@ describe('Reviews Route', function() {
 		},
 		{
 			email: "joe@email.com",
-			password: "supersecret"
+			password: "supersecret",
+			admin: true
 		}])
 		.then(function(users) {
 			createdUsers = users;
+			// We'll need the unhashed passwords for testing authenitcation later
+			createdUsers[0].password = 'secret';
+			createdUsers[1].password = 'supersecret';
 
 			return Category.create([{
 				name: "Partyboats",
@@ -91,12 +95,9 @@ describe('Reviews Route', function() {
 		})
 		.then(function(reviews) {
 			createdReviews = reviews;
-			console.log("created reviews: ", createdReviews);
 		})
 		.then(function() {done();})
 		.then(null, done);
-
-
 	});
 
 	afterEach('Clear test database', function(done) {
@@ -120,7 +121,164 @@ describe('Reviews Route', function() {
 					done();
 				});
 		});
+	});
+
+	describe('Fetch one review', function() {
+
+		var guestAgent;
+
+		beforeEach('Create guest agent', function() {
+			guestAgent = supertest.agent(app);
+		});
+
+		it('should return the correct review', function(done) {
+			guestAgent.get('/api/reviews/' + createdReviews[0]._id)
+				.expect(200)
+				.end(function(err, response) {
+					if (err) return done(err);
+					expect(response.body.content).to.equal(createdReviews[0].content);
+					done();
+				});
+		});
+
+		it('should return 404 if review does not exist', function(done) {
+			guestAgent.get('/api/reviews/' + 'abcd')
+				.expect(404)
+				.end(done);
+		});
 
 	});
 
+	describe('Post a new review', function() {
+
+		describe('Unauthicated requests', function() {
+
+			var guestAgent;
+			var newReviewContent = "I emphasize this is good!";
+
+			beforeEach('Create guest agent', function() {
+				guestAgent = supertest.agent(app);
+			});
+
+			it('should reject the post if you are not authenticated', function(done) {
+				guestAgent.post('/api/reviews')
+					.send({
+						product: createdProducts[0]._id,
+						user: createdUsers[0]._id,
+						content: newReviewContent,
+						starRating: 4
+					})
+					.expect(401)
+					.end(done);
+			});
+
+		});
+
+		describe('Authenticated requests', function() {
+			var loggedInAgent;
+
+			// beforeEach('Create a user', function(done) {
+			// 	User.create(userInfo, done);
+			// });
+
+			beforeEach('Create loggedIn user agent and authenticate', function (done) {
+				loggedInAgent = supertest.agent(app);
+				loggedInAgent.post('/login').send({
+					email: createdUsers[0].email,
+					password: createdUsers[0].password
+				}).end(done);
+			});
+
+			it('should return status 200 and the created review', function(done) {
+				var newReviewContent = "I emphasize this is good!";
+
+				loggedInAgent.post('/api/reviews')
+					.send({
+						product: createdProducts[0]._id,
+						user: createdUsers[0]._id,
+						content: newReviewContent,
+						starRating: 4
+					}) 
+					.expect(200)
+					.end(function(err, response) {
+						expect(response.body.content).to.equal(newReviewContent);
+						done();
+					});
+			});
+		});
+	});
+
+	describe('Delete a review', function() {
+
+		describe('Unauthicated requests', function() {
+
+			var guestAgent;
+
+			beforeEach('Create guest agent', function() {
+				guestAgent = supertest.agent(app);
+			});
+
+			it('should not allow a guest to delete any review', function(done) {
+				guestAgent.delete('/api/reviews/' + createdReviews[0]._id)
+					.expect(401)
+					.end(done);
+			});
+
+		});
+
+
+		describe('Authenticated requests', function() {
+			var loggedInAgent;
+
+			beforeEach('Create loggedIn user agent and authenticate', function (done) {
+				loggedInAgent = supertest.agent(app);
+				loggedInAgent.post('/login').send({
+					email: createdUsers[0].email,
+					password: createdUsers[0].password
+				}).end(done);
+			});
+
+			// review 1 is not created by user 0 -> 401
+			it('should only allow to delete your own reviews as a user', function(done) {
+				loggedInAgent.delete('/api/reviews/' + createdReviews[1]._id)
+					.expect(401)
+					.end(done);
+			});
+
+			// review 0 is created by user 0 ->204
+			it('should return 204 if the review is deleted', function(done) {
+				loggedInAgent.delete('/api/reviews/' + createdReviews[0]._id)
+					.expect(204)
+					.end(function(err, response) {
+						loggedInAgent.get('/api/reviews/' + createdReviews[0]._id)
+							.expect(404)
+							.end(done);
+					});
+			});
+		});
+
+		describe('Admin requests', function() {
+			var adminAgent;
+
+			// user 0 is an admin
+			beforeEach('Create admin user agent and authenticate', function (done) {
+				adminAgent = supertest.agent(app);
+				adminAgent.post('/login').send({
+					email: createdUsers[1].email,
+					password: createdUsers[1].password
+				}).end(done);
+			});
+
+			// review 2 is created by user 0
+			it('should allow an admin to delete any review', function(done) {
+				adminAgent.delete('/api/reviews/' + createdReviews[2]._id)
+					.expect(204)
+					.end(function(err, response) {
+						adminAgent.get('/api/reviews/' + createdReviews[2]._id)
+							.expect(404)
+							.end(done);
+					});
+			});	
+		});
+	});
 });
