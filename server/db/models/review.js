33f75782185b2f2a4e0eee7142de3031c7b0842e
khumphrey <khumphrey7890@gmail.com>
2016-02-29 21:42:11 -0500

    review schema

---
+++ server/db/models/review.js
@@ -0,0 +1,20 @@
+'use strict';
+const mongoose = require('mongoose');
+
+var reviewSchema = new mongoose.Schema({
+	product: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Product',
+		required: true
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'User',
+		required: true
+	},
+	content: {
+		type: String,
+		required: true,
+		minlength: 10
+	}
+});
