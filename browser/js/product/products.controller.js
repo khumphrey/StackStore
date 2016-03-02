f73e44ec43fe48ce8b4bb842d52f1943e2393f6d
apackin <apackin@gmail.com>
2016-03-02 16:53:45 -0500

    interpolated categories from products

--- browser/js/product/products.controller.js
+++ browser/js/product/products.controller.js
@@ -1,7 +1,15 @@
 app.controller('ProductsController', function ($scope, products) {
 	$scope.products = products;
-	$scope.categories = ['motor boat', 'cruise ship', 'pirate ship'];
-	
+	$scope.categories = [];
+	$scope.priceSearch = 900;
+	$scope.quantitySearch = 1;
+	// product.categories =
+// [{"_id":"56d6211a41380ba6524bf3e7","name":"pirate ship","__v":0}],
+	products.forEach(function(elem){
+		elem.categories.forEach(function(categoryObj){
+			if ($scope.categories.indexOf(categoryObj.name)===-1)$scope.categories.push(categoryObj.name);
+		});
+	});
 	
 	$scope.categoryModel = {};
 
