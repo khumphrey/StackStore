7757f6834495c3157b5d2074707d9acde3a9f414
Vincent Leombruno <vleombruno42@gmail.com>
2016-03-04 13:08:03 -0500

    continuing work on cart page

--- browser/js/cart/cart.controller.js
+++ browser/js/cart/cart.controller.js
@@ -2,4 +2,18 @@
 
 app.controller('CartController', function ($scope, cart, CartFactory) {
 	$scope.cart = cart;
+
+	$scope.update = function (item) {
+		CartFactory.updateItem(item.product._id, item.newQuantity)
+		.then(function () {
+			item.quantity = item.newQuantity;
+			item.newQuantity = "";
+		})
+	}
+	$scope.remove = function (item, index) {
+		CartFactory.removeItem(item.product._id)
+		.then(function () {
+			return $scope.cart.splice(index, 1);
+		})
+	}
 })
