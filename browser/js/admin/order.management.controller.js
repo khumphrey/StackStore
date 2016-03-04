d50fe436f91f888de7e4a4ba509e77c9ae46c220
Patrick <patsneep@gmail.com>
2016-03-03 20:35:47 -0500

    added jquery for dropdown button

--- browser/js/admin/order.management.controller.js
+++ browser/js/admin/order.management.controller.js
@@ -1,3 +1,26 @@
 app.controller('OrderManagementCtrl', function($scope, orders) {
 	$scope.orders = orders;
+
+	$scope.createDropdownItems = function(order) {
+		var possibleItems = {
+			Created: ["Processing"],
+			Processing: ["Cancelled", "Completed"],
+			Cancelled: [],
+			Completed: []
+		}
+		console.log(possibleItems[order.orderStatus])
+
+		return possibleItems[order.orderStatus];
+	};
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+
 });
