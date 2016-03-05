328930c77e06df25f1db2272ca1a581a9e7f6704
Vincent Leombruno <vleombruno42@gmail.com>
2016-03-05 17:03:27 -0500

    cart functionality updates

--- browser/js/cart/cart.controller.js
+++ browser/js/cart/cart.controller.js
@@ -1,21 +1,38 @@
 'use strict';
 
 app.controller('CartController', function ($scope, cart, CartFactory) {
-	$scope.cart = cart;
+	$scope.cart = cart;	
 
-	
+	var total = function() {
+		var tot = 0;
+		for(var i=0; i<$scope.cart.length; i++) {
+			tot += ($scope.cart[i].product.price * $scope.cart[i].quantity);
+		}
+		return tot;
+	};
+
+	$scope.total = total();
 
 	$scope.update = function (item) {
-		CartFactory.updateItem(item.product._id, item.newQuantity)
-		.then(function () {
-			item.quantity = item.newQuantity;
-			item.newQuantity = "";
-		})
-	}
+		if(item.quantity !== "") {
+			CartFactory.updateItem(item.product._id, item.quantity)
+			.then(function () {
+				// item.quantity = item.newQuantity;
+				// item.newQuantity = "";
+				$scope.total = total();
+			});
+		}
+	};
+
 	$scope.remove = function (item, index) {
 		CartFactory.removeItem(item.product._id)
 		.then(function () {
 			return $scope.cart.splice(index, 1);
-		})
+		});
+	};
+
+	$scope.change = function (item, num) {
+		item.quantity += num;
+		$scope.update(item);
 	}
-})
+});
